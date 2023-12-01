bf1868a2957d54b8a11fe8dd24a9514100c6c47b
hopesf <gecin18@gmail.com>
2023-12-01 14:29:58 +0300

    updated all packages

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,18 +1,20 @@
 module.exports = {
   extends: [
-    'airbnb-base',
-    'plugin:eslint-comments/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
+    "airbnb-base",
+    "plugin:eslint-comments/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
   ],
   env: {
     jest: true,
+    node: true,
+    browser: true,
   },
-  plugins: ['import', 'prettier', 'jest', 'eslint-comments'],
+  plugins: ["import", "prettier", "jest", "eslint-comments"],
   rules: {
-    'prettier/prettier': ['error'],
+    "prettier/prettier": ["error"],
   },
 };
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,23 +1,19 @@
 module.exports = {
-  extends: [
-    'next',
-    '@arastaitc/eslint-config-base-typescript',
-    'prettier/react'
-  ],
-  plugins: ['jsx-a11y'],
+  extends: ["@next/eslint-config-next", "@arastaitc/eslint-config-base-typescript", "prettier/react"],
+  plugins: ["jsx-a11y"],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'react/prop-types': 'off',
-    'react/button-has-type': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+    "react/prop-types": "off",
+    "react/button-has-type": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
       {
         devDependencies: [
-          '.storybook/**/*.js',
-          'config-overrides.js',
-          'src/setupTests.ts',
-          'src/components/**/*.stories.tsx',
-          'src/**/*.test.{ts,tsx}',
+          ".storybook/**/*.js",
+          "config-overrides.js",
+          "src/setupTests.ts",
+          "src/components/**/*.stories.tsx",
+          "src/**/*.test.{ts,tsx}",
         ],
       },
     ],
